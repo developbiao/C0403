21019d5c35c5a2eb333262478bb92efe3ddcd786
gongbiao <ggongbiao@gmail.com>
2015-11-05 20:13:28 +0800

    unsigned 是修饰int最高位的

---
+++ 1105/demo01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	//unsigned int == unsigned
+	//unsigned 无符号:正数,0,负数
+
+	unsigned int b = 10;
+	unsigned int b2 = 10;
+
+	long unsigned int c = 34343;
+	long unsigned int c2 = 3443;
+
+	//unsigned 修改int最高位
+	return 0;
+}
